d74b5bff68a761fac53ea57637cccfd98bd212bb
Pumpkinjam <rnjsl1212@naver.com>
2023-05-17 16:31:21 +0900

    initial commit

---
+++ src/test/java/com/comnet/CNProject/CnProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comnet.CNProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CnProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
