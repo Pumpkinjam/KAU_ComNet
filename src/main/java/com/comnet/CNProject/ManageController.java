0dc90b1bce049183b16f2a52e7f0fec12805887c
630hyuk <630hyuk@naver.com>
2023-05-18 22:54:37 +0900

    login 이후 manage 페이지에서 스무고개 게임 진행 페이지 생성 (예/아니요 1번 입력에 대한 것만 구현)

---
+++ src/main/java/com/comnet/CNProject/ManageController.java
@@ -0,0 +1,13 @@
+package com.comnet.CNProject;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ManageController {
+
+    @GetMapping("/manage")
+    public String showManagePage() {
+        return "manage.html";
+    }
+}
